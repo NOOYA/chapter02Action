4f916370cd8124490a9d0f878971640c6fe7242b
熊谷裕也 <kumagaiyuuya@kumagaiyuuyas-MacBook-Pro.local>
2016-05-23 09:06:56 +0900

    work at home

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,28 @@ package controllers
 
 import play.api.mvc._
 import models._
+import views._
 
 object Application extends Controller {
   // Actionは関数
-  def index = Action {
-    Ok(views.html.main)
-  }
+//  def index = Action {
+//   Ok(views.html.main)
+//  }
 
   def listArtist = Action{
-  	Ok(views.html.home(Artist.fetch))
+    Ok(views.html.home(Artist.fetch))
+  }
+  def fetchArtistByName(name:String) = Action{
+    Ok(views.html.home(Artist.fetchByName(name)))
+  }
+
+  def search(name:String , country:String): Action ={
+    var result = Artist.fetchByNameOrCountry(name,country)
+    if(result.isEmpty){
+      NoContent
+    }
+    else{
+      Ok(views.html.home(result))
+    }
   }
 }
